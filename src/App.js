b2b1eac5bdad58ce02b81dd4ee3e5a08db99e627
Caleb Bush <calebmatthewbush@gmail.com>
2023-01-19 15:58:31 -0600

    Add submit component for form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Personal from './personal'
 import Educational from './educational'
 import Practical from './practical'
+import Submit from './submit'
 
 
 class App extends React.Component {
@@ -12,6 +13,7 @@ class App extends React.Component {
                 <Personal />
                 <Educational />
                 <Practical />
+                <Submit />
             </div>
         )
     }
---
+++ src/submit.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Submit extends React.Component{
+    render(){
+        return (
+            <button type="submit">Submit</button>
+        )
+    }
+}
+
+export default Submit
